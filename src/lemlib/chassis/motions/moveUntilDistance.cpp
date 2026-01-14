113b17d8aa0c3f4a48319d326e215112b445f20a
Infinity Chicken <8zsc2014@gmail.com>
2026-01-13 18:55:37 -0800

    ack bugfix

--- src/lemlib/chassis/motions/moveUntilDistance.cpp
+++ src/lemlib/chassis/motions/moveUntilDistance.cpp
@@ -3,14 +3,14 @@
 #include "lemlib/util.hpp"
 #include "pros/misc.hpp"
 
-float mmToIn(float mm) {
+float mmToIn2(float mm) {
     return mm / 25.4;
 }
 
 void lemlib::Chassis::moveUntilDistance(float exitDist, float x, float y, float timeout, MoveToPointParams params) {
     this->moveToPoint(x, y, timeout, params, true); 
 
-    while(mmToIn(this->distSensors.front.distance.get()) > exitDist && this->isInMotion()) {
+    while(mmToIn2(this->distSensors.front.distance.get()) > exitDist && this->isInMotion()) {
         pros::delay(10);
     }
 
