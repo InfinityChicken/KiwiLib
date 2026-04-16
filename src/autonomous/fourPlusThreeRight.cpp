57fc7141a4a7181a87cc893fc5d1fe81c4509c0a
aakankshP <aakankshspranavpasupuleti@gmail.com>
2026-04-16 14:27:40 -0700

    4+3 right stuff

--- src/autonomous/fourPlusThreeRight.cpp
+++ src/autonomous/fourPlusThreeRight.cpp
@@ -16,45 +16,46 @@ void fourPlusThreeRight() {
     wingState = 0;
     chassis.turnToHeading(182, 1000);
     chassis.moveDistance(10, 1000, {.minSpeed = 127 * 0.5});
-    chassis.sendVoltage(6000, 300);
+    chassis.sendVoltage(6000, 350);
     chassis.turnToHeading(180, 1000);
     chassis.distanceReset('L', 'F');
 
     //score long
-    chassis.moveToPoint(46, -27, 1000, {.forwards = false, .minSpeed = 100, .earlyExitRange = 8});
-    chassis.moveToPoint(46, -27, 1000, {.forwards = false, .maxSpeed = 80}, true);
-    chassis.waitUntil(5);
+    chassis.moveToPoint(48, -25, 1000, {.forwards = false, .minSpeed = 100});
     intakeState = 3;
     trapdoorState = 1;
-    chassis.waitUntilDone();
-    chassis.sendVoltage(-12000 * 0.5, 500);
+    chassis.sendVoltage(-12000 * 0.5, 1350);
+    chassis.setPose(1, -1, chassis.getPose().theta);
     chassis.distanceReset('L', 'F');
     scraperState = 0;
 
-    //wing
-    chassis.moveToPoint(33, -40, 1000);
-    trapdoorState = 0;
-    chassis.turnToHeading(0, 1000);
-    chassis.moveToPoint(32, -13, 1000);
+    //wing copied from 7right
+    chassis.sendVoltage(12000, 150);
+    chassis.moveToPoint(35.25, -40, 1000, {.minSpeed = 60, .earlyExitRange = 4});
+    chassis.turnToHeading(-10, 500, {.minSpeed = 60, .earlyExitRange = 2});
+    chassis.moveToPoint(33.5, -13, 1000);
     chassis.turnToHeading(-15, 1000, {.minSpeed = 110});
+    chassis.setBrakeMode(pros::E_MOTOR_BRAKE_HOLD);
 
     //delay and exit long
     pros::delay(3000);
     intakeState = 1;
     wingState = 1;
+    chassis.setBrakeMode(pros::E_MOTOR_BRAKE_BRAKE);
+    chassis.moveDistance(5, 1000);
     chassis.setBrakeMode(pros::E_MOTOR_BRAKE_COAST);
     chassis.turnToPoint(21, -24, 1000);
     chassis.setBrakeMode(pros::E_MOTOR_BRAKE_BRAKE);
 
     //block grab
     chassis.moveToPoint(21, -24, 1000, {}, true);
-    chassis.waitUntil(4);
+    chassis.waitUntil(4); //tune
     scraperState = 1;
     chassis.waitUntilDone();
 
     //score mid
     chassis.turnToHeading(-45, 1000);
     scraperState = 0;
-    chassis.moveToPoint(2, -7, 1000, {.forwards = false, .maxSpeed = 60});
+    chassis.moveToPoint(2, -7, 1000, {.maxSpeed = 60});
     intakeState = 2;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ void autonomous() {
 
 	// sevenRight();
 	// sevenLeft();
-	// fourPlusThreeRight
+	fourPlusThreeRight();
 	// fourPlusThreeLeft();
 	
 }
