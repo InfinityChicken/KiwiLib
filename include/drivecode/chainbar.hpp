e8c33de16b383fb1fef38abca6f7f31111797add
Infinity Chicken <8zsc2014@gmail.com>
2026-09-04 18:02:44 -0700

    chainbar code done

--- include/drivecode/chainbar.hpp
+++ include/drivecode/chainbar.hpp
@@ -2,37 +2,14 @@
 #include "pros/misc.h"
 #include "drivecode/objects.hpp"
 
-//
 extern int chainBarState;
 
 // positions, tune
+extern int targetPos;
 extern int loadPos;
 extern int neutralPos;
 extern int upperScorePos;
 extern int lowerScorePos;
 
-// cascade variables??
-// extern float chainBarLoad;
-// extern float chainBarScore;
-// extern float cascadeIncrement;
-
-// extern int incrementWorks;
-// extern float cascadeHeight;
-
-// extern float pinDimension;
-// extern float stackDimension;
-
-// extern int heightConstant;
-// extern int currentLevel;
-// extern int targetInches;
-// chainBarEasy is a function made just to simplify code
-// so we can just call this instead of doing so multiple times, 
-// especially in detailed commands such as pinFromWall.cpp
-void chainBarEasy(float target);
-
-void updateCascade();
+void runChainBar();
 void updateChainBar();
-void updateCascadeFunctions();
-
-void runCascade();
-void cascadeEasy();
--- src/drivecode/chainbar.cpp
+++ src/drivecode/chainbar.cpp
@@ -1,5 +1,8 @@
 #include "drivecode/chainbar.hpp"
 
+bool cbUpPressed = false;
+bool cbDownPressed = false;
+
 void updateChainBar() {
     if (controller.get_digital(cbUpControl)) {
         if (!cbUpPressed) {
@@ -12,16 +15,25 @@ void updateChainBar() {
     if (controller.get_digital(cbDownControl)) {
         if (!cbDownPressed) {
             chainBarState -= 1;
-            chainBarState = std::min(chainBarState, 0);
+            chainBarState = std::max(chainBarState, 0);
         }
         cbDownPressed = true;
     } else cbDownPressed = false;
 }
 
-void runChainBar(float target) {
+void runChainBar() {
     while(true) {
+        switch(chainBarState) {
+            case(0): targetPos = neutralPos;
+            case(1): targetPos = loadPos;
+            case(2): targetPos = upperScorePos;
+            case(3): targetPos = lowerScorePos;
+            break;
+        }
+
         float pos = chainBarRotation.get_position();
-        float power = chainBarPID.update(chainBarRotation.get_position(), true);
+        float error = targetPos - pos;
+        float power = chainBarPID.update(error, true);
 
         if(std::abs(power) > 127) {
             if(power < 0) {
@@ -32,7 +44,6 @@ void runChainBar(float target) {
         }
 
         chainBar.move(power);
-
         pros::delay(10);
     }
 }
